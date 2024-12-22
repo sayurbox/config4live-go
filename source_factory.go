1f61847102677103c05990788b8507d824716e80
Lugas Septiawan <lugas.septiawan@sayurbox.com>
2024-12-23 00:05:02 +0700

    Implement source factory

---
+++ source_factory.go
@@ -0,0 +1,112 @@
+package config4live
+
+import (
+	"strings"
+
+	"github.com/sayurbox/config4live-go/grpc"
+	"github.com/sayurbox/config4live-go/http"
+	"github.com/sayurbox/config4live-go/internal"
+)
+
+type SourceProperty struct {
+	url          string
+	hystrixParam *internal.HystrixParams
+}
+
+// SourceOption is mapper of source property
+type SourceOption func(s *SourceProperty)
+
+// WithURL set http url
+func WithURL(url string) SourceOption {
+	return func(s *SourceProperty) {
+		s.url = url
+	}
+}
+
+// WithHystrixTimeout set hystrix timeout
+func WithHystrixTimeout(timeout int) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.Timeout = timeout
+	}
+}
+
+// WithHystrixSleepWindow set hystrix sleep_window
+func WithHystrixSleepWindow(sleepWindow int) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.SleepWindow = sleepWindow
+	}
+}
+
+// WithHystrixRequestVolumeThreshold set hystrix request_volume_threshold
+func WithHystrixRequestVolumeThreshold(requestVolume int) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.RequestVolumeThreshold = requestVolume
+	}
+}
+
+// WithHystrixErrorPercentThreshold set hystrix error_percent_threshold
+func WithHystrixErrorPercentThreshold(errorPercent int) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.ErrorPercentThreshold = errorPercent
+	}
+}
+
+// WithHystrixMaxConcurrentRequests set hystrix max_concurrent_requests
+func WithHystrixMaxConcurrentRequests(maxRequest int) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.MaxConcurrentRequests = maxRequest
+	}
+}
+
+// WithHystrixCommandName set hystrix command name
+func WithHystrixCommandName(commandName string) SourceOption {
+	return func(s *SourceProperty) {
+		if s.hystrixParam == nil {
+			s.hystrixParam = &internal.HystrixParams{}
+		}
+		s.hystrixParam.Name = commandName
+	}
+}
+
+// NewSource create configuration source
+func NewSource(opts ...SourceOption) Source {
+	s := &SourceProperty{}
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	if strings.HasPrefix(s.url, "http") {
+		return http.NewHttpSource(
+			http.WithURL(s.url),
+			http.WithHystrixTimeout(s.hystrixParam.Timeout),
+			http.WithHystrixErrorPercentThreshold(s.hystrixParam.ErrorPercentThreshold),
+			http.WithHystrixSleepWindow(s.hystrixParam.SleepWindow),
+			http.WithHystrixRequestVolumeThreshold(s.hystrixParam.RequestVolumeThreshold),
+			http.WithHystrixMaxConcurrentRequests(s.hystrixParam.MaxConcurrentRequests),
+			http.WithHystrixCommandName(s.hystrixParam.Name))
+	}
+
+	return grpc.NewGrpcSource(
+		grpc.WithURL(s.url),
+		grpc.WithHystrixTimeout(s.hystrixParam.Timeout),
+		grpc.WithHystrixErrorPercentThreshold(s.hystrixParam.ErrorPercentThreshold),
+		grpc.WithHystrixSleepWindow(s.hystrixParam.SleepWindow),
+		grpc.WithHystrixRequestVolumeThreshold(s.hystrixParam.RequestVolumeThreshold),
+		grpc.WithHystrixMaxConcurrentRequests(s.hystrixParam.MaxConcurrentRequests),
+		grpc.WithHystrixCommandName(s.hystrixParam.Name))
+}
